4af98b19b5f36cefbf109f0ae2de4ba47a45e850
0AkshathaV <akshatha.v@s.amity.edu>
2025-04-19 12:31:35 +0530

    second code

--- starting_code.py
+++ starting_code.py
@@ -4,5 +4,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models
+
 import sequential, load_model
 from tensorflow.keras.layers
+import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
