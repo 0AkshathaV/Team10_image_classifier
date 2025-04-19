6845d76e2386aa29f6103948a838c232bbb398f1
0AkshathaV <akshatha.v@s.amity.edu>
2025-04-19 12:42:09 +0530

    third code

--- starting_code.py
+++ starting_code.py
@@ -14,3 +14,5 @@ from tensorflow.keras.models
 import ImageDataGenerator, load_img, img_to_array
 from tensorflow.keras.callbacks
 import ModelCheckpoint, Early Stopping
+from sklearn.metrics
+import classification_report,confusion_matrix
