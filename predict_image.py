cb040f31e2338bf68a5e929a0311c3f1d4ee3d15
0AkshathaV <akshtaha.v@s.amity.edu>
2025-05-14 14:01:09 +0530

    predicted class

--- predict_image.py
+++ predict_image.py
@@ -1,2 +1,6 @@
 img_array = np.expand_dims(img_array, axis=0)
 prediction = model.predict(img_array)
+
+predicted_class = np.argmax(prediction)
+print(f"Predicted class index: {predicted_class}")
+return predicted_class
