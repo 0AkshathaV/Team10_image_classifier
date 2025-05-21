ac7bd69050d56601a5b74e1449826ce6d245407b
0AkshathaV <akshtaha.v@s.amity.edu>
2025-05-21 13:54:57 +0530

    removed

--- predict_image.py
+++ predict_image.py
@@ -5,7 +5,6 @@ def predict_image(img_path):
 
 img_array = np.expand_dims(img_array, axis=0)
 prediction = model.predict(img_array)
-
 predicted_class = np.argmax(prediction)
 print(f"Predicted class index: {predicted_class}")
 return predicted_class
